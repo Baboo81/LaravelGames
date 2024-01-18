07c6c345445fb00e4aa64a8f1df61813ba621c59
Baboo81 <magicbaboo@gmail.com>
2024-01-18 19:38:26 +0100

    CSS

--- resources/views/navBar/navBar.blade.php
+++ resources/views/navBar/navBar.blade.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-expand-lg bg-dark">
+<nav class="navbar navbar-expand-lg">
     <div class="container">
       <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,13 +8,13 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-                <a class="nav-link @if(Request::route()->getName() == 'app_home') acitve @endif" aria-current href="{{ route('app_home') }}">Home</a>
+                <a class="nav-link mx-5 @if(Request::route()->getName() == 'app_home') acitve @endif" aria-current href="{{ route('app_home') }}">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link @if(Request::route()->getName() == 'app_about') acitve @endif" href="{{ route('app_about') }}">About</a>
+                <a class="nav-link mx-5 @if(Request::route()->getName() == 'app_about') acitve @endif" href="{{ route('app_about') }}">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link @if(Request::route()->getName() == 'app_dashboard') acitve @endif" href="{{ route('app_dashboard') }}">Dashboard</a>
+                <a class="nav-link mx-5 @if(Request::route()->getName() == 'app_dashboard') acitve @endif" href="{{ route('app_dashboard') }}">Dashboard</a>
             </li>
         </ul>
       </div>
