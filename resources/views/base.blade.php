0c11db11c6355a324f8ec8c2d38a807cceb0a248
Baboo81 <magicbaboo@gmail.com>
2024-01-22 10:55:45 +0100

    Created rep js

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,7 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <link rel="stylesheet" href="{{ asset('assets/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/styles/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/styles/user.css') }}">
     </head>
 
     <body>
--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,2 +1,3 @@
 <script src="{{ asset('assets/lib/bootstrap/js/bootstrap.js') }}" defer></script>
 <script src="{{ asset('assets/lib/jQuery/jQuery.js') }}" defer></script>
+<script src="{{ asset('assets/scripts/user/user.js') }}" defer></script>
