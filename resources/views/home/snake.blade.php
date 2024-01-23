3b05151212ae9e595b5978746052097465ff4688
Baboo81 <magicbaboo@gmail.com>
2024-01-23 17:35:31 +0100

    snake.blade.php

--- resources/views/home/snake.blade.php
+++ resources/views/home/snake.blade.php
@@ -3,12 +3,10 @@
 @section('title', 'snake')
 
 @section('content')
-<h1>Snake</h1>
-
 <div class="container">
     <div class="row">
         <div class="col-md-6 py-5">
-            <h2 class="text-center py-5">Snake</h2>
+            <h1 class="text-end py-5">Snake</h1>
             <canvas width="400" height="400"></canvas>
         </div>
         <div class="col-md-6 snake">
