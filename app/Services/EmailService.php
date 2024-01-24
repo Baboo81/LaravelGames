f59679407babe45ba930506881813348ab2015f1
Baboo81 <magicbaboo@gmail.com>
2024-01-24 20:11:03 +0100

    Created ServicesEmail

---
+++ app/Services/EmailService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+//Création d'un service pour l'envoie d'un mail de vérification
+class EmailService
+{
+    protected $app_name;
+    protected $username;
+    protected $password;
+    protected $port;
+    protected $host;
+
+
+}
+
+?>
