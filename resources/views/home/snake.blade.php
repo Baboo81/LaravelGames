0b972a2e70775eea10aa9116f933fd5a02fe9d48
Baboo81 <magicbaboo@gmail.com>
2024-01-30 20:35:19 +0100

    css

--- resources/views/home/snake.blade.php
+++ resources/views/home/snake.blade.php
@@ -5,12 +5,11 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-6 py-5">
-            <h1 class="text-end py-5">Snake</h1>
-            <canvas width="400" height="400"></canvas>
-        </div>
-        <div class="col-md-6 snake">
-            <img src="" alt="logo">
+        <div class="col-md-12">
+            <section class="snakeBg">
+                <h1 class="text-center py-5 mainTitle">Snake</h1>
+                <canvas width="400" height="400"></canvas>
+            </section>
         </div>
     </div>
 </div>
