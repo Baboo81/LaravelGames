1bd91add7c2ebb7bcba5a065f1a366e8e6e5ea4f
Baboo81 <magicbaboo@gmail.com>
2024-01-30 13:57:48 +0100

    Home

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,8 +3,15 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1 class="text-center py-5">Home</h1>
-    <section class="home">
-
-    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <section class="home">
+                    <div>
+                        <h1 class="text-center py-5 homeTitle">Welcome To Gamers Old School</h1>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
 @endsection
