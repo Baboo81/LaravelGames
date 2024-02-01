5beb91e60adac9b39ede47685219b34b4cd19a51
Baboo81 <magicbaboo@gmail.com>
2024-02-01 15:01:45 +0100

    Debug

--- resources/views/home/flyingDuky.blade.php
+++ resources/views/home/flyingDuky.blade.php
@@ -6,11 +6,11 @@
     <div class="container">
         <div class="row">
             <div class="col-mg-6">
-                <h1 class="text-center py-5">Duky</h1>
+                <h1 class="text-center py-5">Blue Bird</h1>
                 <div class="background"></div>
-                <img src="/games/public/assets/img/canard.svg" alt="invalid-img" class="bird" id="bird-1">
+                    <img src="/games/public/assets/img/Bird.png" alt="bird-img" class="bird" id="bird-1">
                     <div class="message">
-                        Enter To Start Game <p><span style="color:red">&uarr;</span> ArrowUp to Control</p>
+                        Enter To Start Game <p><span style="color: red;">&uarr;</span> ArrowUp to Control</p>
                     </div>
                     <div class="score">
                         <span class="score_title"></span>
@@ -19,5 +19,4 @@
                 </div>
             </div>
         </div>
-    </div>
 @endsection
